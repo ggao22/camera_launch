49ba9464e10d900174d81c7de8f227f1322fd4b8
ggao22 <g.gao@wustl.edu>
2022-11-20 22:30:21 -0600

    clustering test

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -12,15 +12,31 @@ def generate_launch_description():
         ]
     )
 
+    n = 8
+
     inv_mux_node = Node(
         package="inv_mux",
         executable="inv_mux",
         parameters=[
-            {'num_parall_nodes': 1}
+            {'num_parall_nodes': n}
         ]
     )
 
+    clustering_node = []
+    for i in range(n):
+        clustering_node.append(
+            Node(
+                package="lanenet_postprocess",
+                executable="postprocessor",
+                parameters=[
+                    {'topic': '/parall_'+str(i)}
+                ]
+            )
+        )
+
     ld.add_action(image_process_node)
     ld.add_action(inv_mux_node)
+    for node in clustering_node:
+        ld.add_action(node)
 
     return ld
